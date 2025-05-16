e6688d6c0dbd1cde6f526bb31701b56427ef9b78
melicobra <160254468+melicobra@users.noreply.github.com>
2025-05-16 09:08:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i=0; i < botoes.length; i++){
+    botoes[i].onclick=function(){
+        for (let j=0; j < botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contador = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-07-01T00:00:00");
+const tempoObjetivo2 = new Date("2025-12-31T00:00:00");
+const tempoObjetivo3 = new Date("2025-07-01T00:00:00");
+const tempoObjetivo4 = new Date("2025-11-09T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date ();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+}
